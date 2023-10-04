3dc05f868632ce9cab2bc06613ea4a20e685b7ff
senaerdem <snnrrdm@gmail.com>
2023-10-04 12:40:43 +0300

    refactoring models

--- NetflixClone/NetflixClone/Models/Title.swift
+++ NetflixClone/NetflixClone/Models/Title.swift
@@ -1,5 +1,5 @@
 //
-//  Tv.swift
+//  Movie.swift
 //  NetflixClone
 //
 //  Created by Sena Nur Erdem on 4.10.2023.
@@ -7,11 +7,11 @@
 
 import Foundation
 
-struct TrendingTvResponse: Codable {
-    let results: [Tv]
+struct TrendingTitleResponse: Codable {
+    let results: [Title]
 }
 
-struct Tv: Codable {
+struct Title: Codable {
     let id: Int
     let media_type: String?
     let original_name: String?
