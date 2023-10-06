df8f5637053ab5dd7d6b5a86c7612992290f8a16
senaerdem <snnrrdm@gmail.com>
2023-10-06 10:01:51 +0300

    creating TitlePreviewViewController

--- NetflixClone/NetflixClone/Controllers/Core/HomeViewController.swift
+++ NetflixClone/NetflixClone/Controllers/Core/HomeViewController.swift
@@ -75,6 +75,8 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
             return UITableViewCell()
         }
         
+        cell.delegate = self
+        
         
         switch indexPath.section {
         case Sections.TrendingMovies.rawValue:
@@ -153,5 +155,15 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         let defaultOffset = view.safeAreaInsets.top
         let offset = scrollView.contentOffset.y + defaultOffset
         navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+    }
+}
+
+extension HomeViewController: CollectionViewTableViewCellDelegate {
+    func collectionViewTableViewCellDidTapCell(_ cell: CollectionViewTableViewCell, viewModel: TitlePreviewViewModel) {
+        DispatchQueue.main.async { [weak self] in 
+            let vc = TitlePreviewViewController()
+            vc.configure(with: viewModel)
+            self?.navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
---
+++ NetflixClone/NetflixClone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitlePreviewViewModel.swift
+//  NetflixClone
+//
+//  Created by Sena Nur Erdem on 6.10.2023.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youtubeView: VideoElement
+    let titleOverview: String
+}
